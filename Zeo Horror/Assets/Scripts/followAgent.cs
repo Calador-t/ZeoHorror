161e583faf236d11f5da666faca74dd07f9c0758
teremich <michi.tereschenko@gmail.com>
2020-05-15 13:37:16 +0200

    adding scripts

---
+++ Zeo Horror/Assets/Scripts/followAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class followAgent : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + new Vector3(0, 0.584f, 0);
+    }
+}
