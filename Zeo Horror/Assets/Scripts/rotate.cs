3a2c1230f6a593767edee08c34746c86972e13f8
teremich <michi.tereschenko@gmail.com>
2020-05-16 21:37:28 +0200

    adding a starting menu

---
+++ Zeo Horror/Assets/Scripts/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    private float theta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theta = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, theta, 0);
+    }
+}
